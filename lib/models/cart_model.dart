2871303788454801f47a23eb362e704047a93951
kevinrisqi <kevinrisqi22@gmail.com>
2022-08-21 20:42:02 +0700

    create cart model

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_interview_test/models/menu_model.dart';
+
+class CartModel {
+  int id;
+  MenuModel menu;
+  int quantity;
+
+  CartModel({
+    required this.id,
+    required this.menu,
+    required this.quantity,
+  });
+
+  factory CartModel.fromJson(Map<String, dynamic> json) {
+    return CartModel(
+      id: json['id'] as int,
+      menu: MenuModel.fromMap(json['menu']),
+      quantity: json['quantity'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'menu': menu.toJson(),
+      'quantity': quantity,
+    };
+  }
+}
